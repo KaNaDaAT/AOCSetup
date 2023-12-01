52af5fa9b8786ae31415d9f2a6cc65084bfa03a3
KaNaDaAT <kalian.danzer@gmail.com>
2023-12-01 14:48:06 +0100

    setup: minor setup improvements

--- AOCXXXX/AdventOfCoding/data.py
+++ AOCXXXX/AdventOfCoding/data.py
@@ -2,6 +2,7 @@ from aocd import get_data
 import sys
 import os
 import datetime
+import time
 
 data = "empty"
 year = "XXXX"
@@ -10,42 +11,45 @@ file_path = os.path.dirname(__file__)
 file_path = os.path.join(file_path, "Days", "Day")
 path = ""
 if len(sys.argv) == 2:
-	current_num = str(sys.argv[1]).zfill(2)
-	current_file_path = file_path + current_num
-	day_A = "Day" + current_num + "A"
-	day_B = "Day" + current_num + "B"
-	data_file_A = os.path.join(current_file_path, day_A + suffix_d)
-	data_file_B = os.path.join(current_file_path, day_B + suffix_d)
+    current_num = str(sys.argv[1]).zfill(2)
+    current_file_path = file_path + current_num
+    day_A = "Day" + current_num + "A"
+    day_B = "Day" + current_num + "B"
+    data_file_A = os.path.join(current_file_path, day_A + suffix_d)
+    data_file_B = os.path.join(current_file_path, day_B + suffix_d)
 
-	try:
-		data = get_data(day=int(sys.argv[1]), year=int(year))
+    try:
+        data = get_data(day=int(sys.argv[1]), year=int(year))
 
-		if not os.path.exists(data_file_A):
-			os.makedirs(os.path.dirname(data_file_A), exist_ok=True)
-			with open(data_file_A, 'w') as fp:
-				fp.write(data)
-		if not os.path.exists(data_file_B):
-			os.makedirs(os.path.dirname(data_file_B), exist_ok=True)
-			with open(data_file_B, 'w') as fp:
-				fp.write(data)
-	except:
-		pass
+        if not os.path.exists(data_file_A):
+            os.makedirs(os.path.dirname(data_file_A), exist_ok=True)
+            with open(data_file_A, 'w') as fp:
+                fp.write(data)
+        if not os.path.exists(data_file_B):
+            os.makedirs(os.path.dirname(data_file_B), exist_ok=True)
+            with open(data_file_B, 'w') as fp:
+                fp.write(data)
+    except:
+        pass
 else:
-	for i in range(1, 26):
-		current_num = str(i).zfill(2)
-		current_file_path = file_path + current_num
-		day_A = "Day" + current_num + "A"
-		day_B = "Day" + current_num + "B"
-		data_file_A = os.path.join(current_file_path, day_A + suffix_d)
-		data_file_B = os.path.join(current_file_path, day_B + suffix_d)
+    for i in range(1, 26):
+        current_num = str(i).zfill(2)
+        current_file_path = file_path + current_num
+        day_A = "Day" + current_num + "A"
+        day_B = "Day" + current_num + "B"
+        data_file_A = os.path.join(current_file_path, day_A + suffix_d)
+        data_file_B = os.path.join(current_file_path, day_B + suffix_d)
 
-		try:
-			data = get_data(day=i, year=int(year))
-			os.makedirs(os.path.dirname(data_file_A), exist_ok=True)
-			with open(data_file_A, 'w') as fp:
-				fp.write(data)
-			os.makedirs(os.path.dirname(data_file_B), exist_ok=True)
-			with open(data_file_B, 'w') as fp:
-				fp.write(data)
-		except Exception as e: print(e)
-	
+        try:
+            data = get_data(day=i, year=int(year))
+            os.makedirs(os.path.dirname(data_file_A), exist_ok=True)
+            with open(data_file_A, 'w') as fp:
+                fp.write(data)
+            os.makedirs(os.path.dirname(data_file_B), exist_ok=True)
+            with open(data_file_B, 'w') as fp:
+                fp.write(data)
+        except Exception as e:
+            if "not available yet" in str(e):
+                sys.exit()
+            print(e)
+    time.sleep(100)
--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ from tkinter import simpledialog
 import glob
 import subprocess
 
-
 file_path = os.path.dirname(__file__)
 AOC_path = os.path.join(file_path, "AOCXXXX")
 year = "XXXX"
@@ -14,38 +13,44 @@ year = "XXXX"
 ROOT = tk.Tk()
 ROOT.withdraw()
 if len(sys.argv) < 2:
-	while not year.isnumeric():
-		year = simpledialog.askstring(title="Year", prompt="Which year?")
+    while not year.isnumeric():
+        year = simpledialog.askstring(title="Year", prompt="Which year?")
 else:
-	year = str(int(sys.argv[1]))
+    year = str(int(sys.argv[1]))
 
 new_AOC_path = os.path.join(file_path, "AOC" + year)
 
 if os.path.exists(new_AOC_path):
-	paths = glob.glob(new_AOC_path + "**/**", recursive=False)
-	if len(paths) != 0:
-		print("Already exists")
-		sys.exit()
-
+    paths = glob.glob(new_AOC_path + "**/**", recursive=False)
+    if len(paths) != 0:
+        print("Already exists")
+        sys.exit()
 
 shutil.copytree(AOC_path, new_AOC_path, dirs_exist_ok=True)
 
 for filename in glob.iglob(new_AOC_path + "**/**", recursive=True):
-	content = ""
-	try:
-		if os.path.isfile(filename):
-			with open(filename, 'r', encoding="utf8") as fp:
-				content = fp.read()
-			content = content.replace("XXXX", year)
-			content = content.replace("YEAR = 0000", "YEAR = " + year)
-			with open(filename, 'w', encoding="utf8") as fp:
-				fp.write(content)
-	except:
-		pass
-	if "XXXX" in filename:
-		shutil.move(filename, filename.replace("XXXX", year))
+    content = ""
+    try:
+        if os.path.isfile(filename):
+            with open(filename, 'r', encoding="utf8") as fp:
+                content = fp.read()
+            content = content.replace("XXXX", year)
+            content = content.replace("YEAR = 0000", "YEAR = " + year)
+            with open(filename, 'w', encoding="utf8") as fp:
+                fp.write(content)
+    except:
+        pass
+    if "XXXX" in filename:
+        shutil.move(filename, filename.replace("XXXX", year))
 
-createDays_path = os.path.join(new_AOC_path, "AdventOfCoding", "Days", "createDays.py")
+createDays_path = os.path.join(new_AOC_path, "AdventOfCoding", "Days",
+                               "createDays.py")
 subprocess.run(["python", createDays_path])
 createData_path = os.path.join(new_AOC_path, "AdventOfCoding", "data.py")
 subprocess.run(["python", createData_path])
+
+git_commands = [
+    "git init", "git add .", f"git commit -m 'AOC {year} Initial commit'"
+]
+for command in git_commands:
+    subprocess.run(command, shell=True)
