67b0315bd0e6fc7711ff613d08138f56bacb0ef8
KaNaDaAT <kalian.danzer@gmail.com>
2023-12-02 15:04:14 +0100

    fix: Fixed shutdown on error

--- AOCXXXX/AdventOfCoding/App.xaml.cs
+++ AOCXXXX/AdventOfCoding/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Threading.Tasks;
+using System.Windows;
 
 namespace AdventOfCoding {
 	/// <summary>
@@ -6,5 +7,28 @@ namespace AdventOfCoding {
 	/// </summary>
 	public partial class App : Application {
 		public static readonly int YEAR = 0000;
+
+		public App()
+		{
+			Dispatcher.UnhandledException += Dispatcher_UnhandledException;
+			TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+			Application.Current.DispatcherUnhandledException += Current_DispatcherUnhandledException;
+		}
+
+		private void Current_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+		{
+			UIConsole.WriteLine(e.Exception);
+			e.Handled = true;
+		}
+
+		private static void Dispatcher_UnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+		{
+			UIConsole.WriteLine(e.Exception);
+		}
+
+		private void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+		{
+			UIConsole.WriteLine(e.Exception);
+		}
 	}
 }
